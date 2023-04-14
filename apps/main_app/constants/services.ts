0ebabe01d8b3052a11d2d6ce2993ca3830e23b8d
Danila Skripko <dalas.prog@gmail.com>
2023-04-14 12:28:32 +0400

    Register queue in main app

---
+++ apps/main_app/constants/services.ts
@@ -0,0 +1,4 @@
+export const FILM_SERVICE = 'FILM'
+export const STAFF_SERVICE = 'STAFF'
+export const COUNTRY_SERVICE = 'COUNTRY'
+export const GENRE_SERVICE = 'GENRE'
--- apps/main_app/src/app.module.ts
+++ apps/main_app/src/app.module.ts
@@ -1,20 +1,33 @@
+import { RmqModule } from '@app/common';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
+import {
+    COUNTRY_SERVICE,
+    FILM_SERVICE,
+    GENRE_SERVICE,
+    STAFF_SERVICE,
+} from '../constants/services';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [
-      ConfigModule.forRoot({
-          isGlobal: true,
-          validationSchema: Joi.object({
-              PORT: Joi.number().required(),
-          }),
-          envFilePath: './apps/ '
-      })
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            validationSchema: Joi.object({
+                PORT: Joi.number().required(),
+                RABBIT_MQ_URI: Joi.string().required(),
+                POSTGRES_URI: Joi.string().required(),
+            }),
+            envFilePath: './apps/ ',
+        }),
+        RmqModule.register({ name: FILM_SERVICE }),
+        RmqModule.register({ name: GENRE_SERVICE }),
+        RmqModule.register({ name: STAFF_SERVICE }),
+        RmqModule.register({ name: COUNTRY_SERVICE }),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
