67afe679917225d446796c7051b898b7cd45283e
Danila Skripko <dalas.prog@gmail.com>
2023-04-14 15:36:01 +0400

    create role guard

--- libs/common/src/auth/roles.guard.ts
+++ libs/common/src/auth/roles.guard.ts
@@ -1,22 +1,38 @@
 import {
     CanActivate,
     ExecutionContext,
+    HttpException,
+    HttpStatus,
     Inject,
     Injectable,
     UnauthorizedException,
 } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { ClientProxy } from '@nestjs/microservices';
 import { catchError, Observable, tap } from 'rxjs';
+import { ROLES_KEY } from './roles-auth.decorator';
 import { AUTH_SERVICE } from './service';
 
 @Injectable()
-export class JwtAuthGuard implements CanActivate {
-    constructor(@Inject(AUTH_SERVICE) private authClient: ClientProxy) {}
+export class RolesGuard implements CanActivate {
+    constructor(
+        private reflector: Reflector,
+        @Inject(AUTH_SERVICE) private authClient: ClientProxy,
+    ) {}
 
     canActivate(
         context: ExecutionContext,
     ): boolean | Promise<boolean> | Observable<boolean> {
         try {
+            const requiredRoles = this.reflector.getAllAndOverride<string[]>(
+                ROLES_KEY,
+                [context.getHandler(), context.getClass()],
+            );
+
+            if (!requiredRoles) {
+                return true;
+            }
+
             const req = context.switchToHttp().getRequest();
             const authHeader = req.headers.authorization;
             const bearer = authHeader.split(' ')[0];
@@ -31,15 +47,17 @@ export class JwtAuthGuard implements CanActivate {
             return this.authClient.send('validate_user', { token }).pipe(
                 tap((res) => {
                     req.user = res;
+                    return res.roles.some((role) =>
+                        requiredRoles.includes(role.value),
+                    );
                 }),
                 catchError(() => {
                     throw new UnauthorizedException();
                 }),
             );
         } catch (e) {
-            throw new UnauthorizedException({
-                message: 'Пользователь не авторизован',
-            });
+            console.log(e);
+            throw new HttpException('Нет доступа', HttpStatus.FORBIDDEN);
         }
     }
 }
