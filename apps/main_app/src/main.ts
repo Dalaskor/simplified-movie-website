1571d434de0eed2325e0eacddf9db1e7edb6221d
Danila Skripko <dalas.prog@gmail.com>
2023-04-19 17:12:37 +0400

    Add swagger

--- apps/main_app/src/main.ts
+++ apps/main_app/src/main.ts
@@ -2,14 +2,25 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const configService = app.get(ConfigService);
+    const swaggerConfig = new DocumentBuilder()
+        .setTitle('Simplified Movie Website')
+        .setDescription('Документация REST API')
+        .setVersion('1.0.0')
+        .addTag('Hotels')
+        .build();
+
 
     app.enableCors();
     app.use(bodyParser.json({ limit: '50mb' }));
     app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('/api/docs', app, document);
 
     await app.listen(configService.get('PORT'));
 }
