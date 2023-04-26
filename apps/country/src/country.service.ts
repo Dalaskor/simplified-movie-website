e153cc84082c3f05881729cceebad9b2bc89a92d
Danila Skripko <dalas.prog@gmail.com>
2023-04-26 11:29:14 +0400

    Refactoring country app

--- apps/country/src/country.service.ts
+++ apps/country/src/country.service.ts
@@ -1,5 +1,10 @@
 import { Country, CreateCountryDto, UpdateCountryDto } from '@app/models';
-import { HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import {
+    BadRequestException,
+    HttpStatus,
+    Injectable,
+    NotFoundException,
+} from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
 import { InjectModel } from '@nestjs/sequelize';
 import { Op } from 'sequelize';
@@ -10,6 +15,11 @@ export class CountryService {
         @InjectModel(Country) private countryRepository: typeof Country,
     ) {}
 
+    /**
+     * Создает страны при заполнении бд.
+     * @param {CreateCountryDto[]} createCountryDtoArray DTO для создания стран из массива.
+     * @returns Country[] - Массив созданных стран
+     */
     async createMany(createCountryDtoArray: CreateCountryDto[]) {
         const countries = await this.countryRepository.bulkCreate(
             createCountryDtoArray,
@@ -19,22 +29,46 @@ export class CountryService {
         return countries;
     }
 
+    /**
+     * Создает одну новую страну.
+     * @param {CreateCountryDto} createCountryDto - DTO для создание страны.
+     * @returns Country - Созданная страна
+     * @throws BadRequestException
+     */
     async create(createCountryDto: CreateCountryDto) {
-        const country = this.countryRepository.create(createCountryDto);
+        const candidate = await this.countryRepository.findOne({
+            where: { name: createCountryDto.name },
+        });
+
+        if (candidate) {
+            throw new RpcException(
+                new BadRequestException('Такая страна уже существует'),
+            );
+        }
+
+        const country = await this.countryRepository.create(createCountryDto);
 
         return country;
     }
 
-    async findAll() {
-        const countries = this.countryRepository.findAll({
-            include: { all: true },
-        });
+    /**
+     * Получить список всех стран.
+     * @returns Country[] - Список найденных стран.
+     */
+    async findAll(): Promise<Country[]> {
+        const countries = await this.countryRepository.findAll();
 
         return countries;
     }
 
+    /**
+     * Найти одну страну по ID.
+     * @param {number} id - Идентификатор страны.
+     * @returns Country - Найденная страна.
+     * @throws NotFoundException
+     */
     async findOne(id: number) {
-        const country = this.countryRepository.findOne({ where: { id } });
+        const country = await this.countryRepository.findOne({ where: { id } });
 
         if (!country) {
             throw new RpcException(new NotFoundException('Страна не найдена'));
@@ -43,8 +77,16 @@ export class CountryService {
         return country;
     }
 
+    /**
+     * Найти страну по названию.
+     * @param {string} name - Название страны.
+     * @returns Country - Найденная страна.
+     * @throws NotFoundException
+     */
     async findByname(name: string): Promise<Country> {
-        const country = this.countryRepository.findOne({ where: { name } });
+        const country = await this.countryRepository.findOne({
+            where: { name },
+        });
 
         if (!country) {
             throw new RpcException(new NotFoundException('Страна не найдена'));
@@ -53,6 +95,11 @@ export class CountryService {
         return country;
     }
 
+    /**
+     * Обновить данные о стране.
+     * @param {UpdateCountryDto} updateCountryDto - DTO для обновления данных о стране.
+     * @returns Country - Обновленная страна.
+     */
     async update(id: number, updateCountryDto: UpdateCountryDto) {
         const country = await this.findOne(id);
 
@@ -63,6 +110,11 @@ export class CountryService {
         return country;
     }
 
+    /**
+     * Удалить страну.
+     * @param {number} id - Идентификатор страны.
+     * @returns Результат выполнения функции.
+     */
     async remove(id: number) {
         const country = await this.findOne(id);
 
@@ -71,6 +123,11 @@ export class CountryService {
         return { status: HttpStatus.OK };
     }
 
+    /**
+     * Получить массив стран по списку названий.
+     * @param {string[]} names - Списк названий стран.
+     * @returns Country[] - Массив найденных стран.
+     */
     async getCountriesByNamesArray(names: string[]): Promise<Country[]> {
         const countries = await this.countryRepository.findAll({
             where: {
